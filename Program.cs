e47b3399baad293b130ee26b1e8132b906b64fbc
yourname <adel@ntime.co>
2020-06-23 19:22:43 +0200

    work with Ado ( removing dapper)

--- Program.cs
+++ Program.cs
@@ -1,8 +1,7 @@
 ﻿
-using Dapper;
-using MySql.Data.MySqlClient;
-using MySqlConnector;
+using System;
 using System.Data;
+using MySql.Data.MySqlClient;
 
 namespace DapperMemsqlConsole
 {
@@ -11,11 +10,25 @@ namespace DapperMemsqlConsole
         static void Main(string[] args)
         {  
             MySqlConnection connection = new MySqlConnection("server=173.249.51.78;Uid=ntime;database=ZilloDb;port=3306;password=P@ssw0rd");
-            var testTableList = connection.Query<TestTable>("Select * From TestTable;").AsList();
+           //var testTableList = connection.Query<TestTable>("Select * From TestTable;").AsList();
+           try
+            {
+                connection.Open();
+                Console.WriteLine($"MySQL version : {connection.ServerVersion}");
+                var cmd = new MySqlCommand("sp_ntime_TestTable_GetAll", connection);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("$Id", 5);
+                var reader = cmd.ExecuteReader();
+            }
+            catch (Exception ex)
+            {
+                
+            }
 
-            var p = new DynamicParameters();
-            p.Add("$Id", 5);
-            var testTableList1 = connection.Query<TestTable>("sp_ntime_TestTable_GetAll", p, commandType: CommandType.StoredProcedure).AsList();
+
+            //var p = new DynamicParameters();
+            // p.Add("$Id", 5);
+            // var testTableList1 = connection.Query<TestTable>("sp_ntime_TestTable_GetAll", p, commandType: CommandType.StoredProcedure).AsList();
       
             // Console.WriteLine("Hello World!");
         }
